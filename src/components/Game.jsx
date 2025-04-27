7210d2def5a4ec0c3a7ccdc836d6a327e8a34b98
Siddhesh Dupare <siddheshdupare92@gmail.com>
2025-04-27 19:25:05 +0530

    Added Go-Back-N Game

---
+++ src/components/Game.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Game = () => {
+  return (
+    <div>
+      <iframe
+        src="/game.html"
+        title="Game"
+        width="100%"
+        height="900"
+        frameBorder="0"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Game;
--- src/pages/GoBackN.jsx
+++ src/pages/GoBackN.jsx
@@ -1,75 +1,16 @@
 import React, { useState } from 'react';
-
+import { motion, AnimatePresence } from 'framer-motion';
 import '../styles/gobackn.css';
 
-const totalPackets = 20;
+import Game from '../components/Game';
 
 const GoBackN = () => {
+
   return (
     <>
-      <div className="wrapper">
-        <div className="btns">
-          <button className="btn send-new-btn">Send New</button>
-          <button className="btn pause-btn">Pause</button>
-          <button className="btn kill-btn">Kill Packet/Ack</button>
-          <button className="btn reset-btn">Reset</button>
-        </div>
-
-        <div className="simulator-wrapper">
-          <div className="wrap sender-wrapper">
-            <div className="packets sender-packets">
-              {[...Array(totalPackets)].map((_, i) => (
-                <div 
-                  key={i}
-                  className="packet"
-                  >
-                    {i}
-                </div>
-              ))}
-            </div>
-
-            <div className="details">
-              <p><b>Sender</b></p>
-            </div>
-          </div>
-
-          <div className="wrap receiver-wrapper">
-            <div className="packets receiver-packets">
-              {[...Array(totalPackets)].map((_, i) => (
-                <div
-                  key={i}
-                  className="packet"
-                  >
-                    {i}
-                </div>
-              ))}
-            </div>
-
-            <div className="details">
-              <p><b>Receiver</b></p>
-            </div>
-          </div>
-        </div>
-
-        <div className="legend-wrapper">
-          <Legend color="bg-cyan-300" label="Packet" />
-          <Legend color="bg-red-500" label="Received" />
-          <Legend color="bg-yellow-300" label="Ack" />
-          <Legend color="bg-blue-400" label="Ack Received" />
-          <Legend color="bg-green-400" label="Selected" />
-        </div>
-      </div>
+      <Game />
     </>
   );
-}
-
-function Legend({ color, label }) {
-  return (
-    <div className="flex items-center gap-2">
-      <div className={`w-4 h-4 rounded-full ${color}`} />
-      <span className="text-sm">{label}</span>
-    </div>
-  );
-}
+};
 
 export default GoBackN;
